3a01c201aea5014e4fdec8f4ed4829385fb02c7b
Jansen Fink <jansenfink@charter.net>
2020-07-26 17:34:30 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -5,6 +5,24 @@ class Book
   end
   
   def title 
+    @title
   end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  def page_count=(num)
+    @page_count = num 
+  end
+  
+  def page_count
+    @page_count
+  end
+  
 end
 
